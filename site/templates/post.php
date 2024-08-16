c1d20e3baba660fbb1d9c14adccafadd30d542f3
prgdw <perengdawe321@gmail.com>
2024-08-16 08:20:42 +0000

    enhance and fix some issues

--- site/templates/post.php
+++ site/templates/post.php
@@ -4,7 +4,7 @@
       <article class="col-md-8 mx-auto">
         <?php snippet('breadcrumb') ?>
         <h1 class="fs-2 fw-bold m-0 mt-3"><?= $page->title()->esc() ?></h1>
-        <div class="hstack gap-3 text-slate mt-2 flex-wrap">
+        <div class="hstack gap-3 text-slate mt-3 flex-wrap">
           <div class="d-flex align-items-center">
             <svg class="flex-shrink-0 me-1" style="width: 1rem; height: 1rem;" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M8 7a4 4 0 1 0 8 0a4 4 0 0 0 -8 0" /><path d="M6 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2" /></svg>
             <?php if ($page->author()): ?>
@@ -33,7 +33,7 @@
       </article>
       <?php if (count($otherPost) > 0): ?>
       <div class="col-md-8 mx-auto mt-4">
-        <span class="d-block fs-4 fw-bold">Berita Lainnya</span class="fs-4 fw-bold">
+        <span class="d-block fs-4 fw-bold"><?= e($page->parent()->title() == "Berita", 'Berita', 'Informasi') ?> Lainnya</span class="fs-4 fw-bold">
         <hr class="border-primary border-3 mt-2 mb-4" style="opacity: 1; width: 48px">
         <div class="row g-4">
           <?php foreach($otherPost as $other): ?>
